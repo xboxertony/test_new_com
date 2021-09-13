b1a36d5c336467976b00be209f89f4939dbcbeb9
Tony Tsai 蔡承翰 [dentsu] <Tony.Tsai@dentsu.com>
2021-09-13 17:43:39 +0800

    crawler

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-print("Hello world")
-print("hello world2")
+import requests as req
+from bs4 import BeautifulSoup
+
+r = req.get("http://127.0.0.1:5500/test.html")
+
+soup = BeautifulSoup(r.text,"html.parser")
+
+for i in soup.find_all("div",class_="delta"):
+    for j in i.select("a"):
+        print(j.string)
